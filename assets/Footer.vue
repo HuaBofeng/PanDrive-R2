5c7e1f83ad004f7c0d8462b7fb65a8b101764180
lanni <148889583+HuaBofeng@users.noreply.github.com>
2026-02-20 09:10:40 +0800

    Update Footer.vue

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -21,7 +21,7 @@
 
     <div class="project-links">
       <strong>项目:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
-        rel="noopener noreferrer">FlareDrive-R2</a>
+        rel="noopener noreferrer">panDrive-R2</a>
       <strong>作者:</strong><a href="https://github.com/willow-god" target="_blank"
         rel="noopener noreferrer">LiuShen</a>
       <strong>初始项目:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
